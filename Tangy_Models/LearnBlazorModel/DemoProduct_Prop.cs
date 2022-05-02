468c238e6bf1b5e9cd56ae0628143e02d191de30
Bouchard, Nicolas <nicolasbouchard@familiprix.com>
2022-05-02 13:51:09 -0400

    Section 4

---
+++ Tangy_Models/LearnBlazorModel/DemoProduct_Prop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.LearnBlazorModel
+{
+    public class DemoProduct_Prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Tangy_Models/LearnBlazorModel/Demo_Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Tangy_Models.LearnBlazorModel
+{
+    public class Demo_Product
+    {
+        public int Id { get;set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public double Price { get; set; }
+
+        public IEnumerable<DemoProduct_Prop> Properties { get; set; }
+    }
+}
